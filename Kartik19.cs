2839362f0f76e603ee79ba025a1390421685dc70
Narendra Chand <137639860+Chandnp3@users.noreply.github.com>
2025-11-05 15:54:35 +0545

    two Num add
    Fixes method names and adds basic sum functionality.

--- Kartik19.cs
+++ Kartik19.cs
@@ -1,1 +1,17 @@
-
+using system;
+namespace Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Writeline("Enter the value of a and b ");
+            a = Convert.Toint32(Console.ReadLine());
+            b = Convert.Toint32(Console.ReadLine());
+            int sum = a + b;
+            Console.Writeline("The sum of a and b is : {0}+{1} = {2}", a, b, sum);
+            Console.ReadLine();
+        }
+    }
+}
